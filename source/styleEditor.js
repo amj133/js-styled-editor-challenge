d63316331e1e19030d912c2dabae2beb17396f3d
amj133 <amj@vt.edu>
2018-04-29 15:25:01 -0600

    challenge complete'

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  // your code goes here.
+  $( "#style_editor" ).on('submit', function(event) {
+    var selector = $( ".css-selector-input" ).val();
+    var property = $( ".css-property-input" ).val();
+    var value = $( ".css-value-input" ).val();
+    $(selector).css(property, value);
+    event.preventDefault();
+  });
 });
